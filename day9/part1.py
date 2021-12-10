35ae8e2b2164bd06a11820ba738d94b8dd6f5742
Isaac <isaac.mauro@gmail.com>
2021-12-09 21:03:34 -0700

    Complete day9 part1

--- day9/part1.py
+++ day9/part1.py
@@ -1,6 +1,24 @@
 import numpy as np
-
-with open('testinput.txt') as input:
-    data = np.array
+data = []
+with open('input.txt') as input:
     for line in input:
-        
+        row = list(map(int, list(line.strip())))
+        data.append(row) 
+data = np.asarray(data, dtype=int)
+sum = 0
+for row in range(np.shape(data)[0]):
+    for col in range(np.shape(data)[1]):
+        # Check above
+        if row > 0:
+            if data[row][col] >= data[row-1][col]: continue
+        # Check below
+        if row < np.shape(data)[0]-1:
+            if data[row][col] >= data[row+1][col]: continue
+        # Check left
+        if col > 0:
+            if data[row][col] >= data[row][col-1]: continue
+        # Check right
+        if col < np.shape(data)[1]-1:
+            if data[row][col] >= data[row][col+1]: continue
+        sum += data[row][col] + 1
+print(sum)
